6f7f3fb27d9a2454ed2f027e7bd4eff729903a5a
Ec3o <2499302531@qq.com>
2024-06-22 19:55:44 +0800

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,11 +1,17 @@
+const path=require('path')
+require('dotenv').config()
+console.log('DBHOST:', process.env.DBHOST);
+console.log('DBUSER:', process.env.DBUSER);
+console.log('DBPASSWD:', process.env.DBPASSWD);
+console.log('DB:', process.env.DB);
+console.log('DBPORT:', process.env.DBPORT);
 const mysql = require('mysql');
-
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'Ec3o',
-    password: 'root',
-    database: 'v1d4r_b10g',
-    port: 3306
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWD,
+    database: process.env.DB,
+    port: process.env.DBPORT
 });
 
 db.connect(err => {
